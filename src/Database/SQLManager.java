faebc5820e67ca9f0e37fa62533e20ca9a80792b
gonkaotic <30638707+gonkaotic@users.noreply.github.com>
2020-10-21 19:21:36 +0200

    Create TableMeasures method
    also created the table for measures directly, can be modified in the future as we see see value types, default values and other things

--- src/Database/SQLManager.java
+++ src/Database/SQLManager.java
@@ -3,6 +3,7 @@ package Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQLManager {
 
@@ -11,17 +12,17 @@ public class SQLManager {
 	public static void main (String[] args) {
 		try {
 			connect("jdbc:sqlite:././Database/covid watchlist.db");
-			//generateDataBase();
+			generateDataBase();
 			disconnect();
 		} catch ( Exception e ) {
-			System.out.println("Error in connecting/generating/disconnecting from the database:" + e.getMessage());
+			System.out.println("Error in connecting/generating/disconnecting from the database: " + e);
 		}
 		
 	}
 	
 	
-	private static void generateDataBase() {
-		System.out.println("Not implemented yet");
+	private static void generateDataBase() throws SQLException {
+		createTableMeasures();
 	}
 
 	public static void connect(String directory) throws ClassNotFoundException, SQLException {
@@ -48,12 +49,18 @@ public class SQLManager {
  */
 	
 	
-	public static void createTablePatients() {
+	public static void createTablePatients() throws SQLException {
 		
 	}
 	
-	public static void createTableMeasures() {
-		
+	public static void createTableMeasures() throws SQLException {
+		Statement stmt1 = c.createStatement();
+		String sql1 = "CREATE TABLE measures " + "(measure_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ " measure_date DATETIME DEFAULT CURRENT_DATETIME," + " ecg TEXT," + " bpm INT NOT NULL," 
+				+ " o2_saturation FLOAT,"+ " temperature FLOAT NOT NULL," 
+				+ " patient_id REFERENCES patients (patient_id) ON UPDATE CASCADE ON DELETE CASCADE );";
+		stmt1.executeUpdate(sql1);
+		stmt1.close();
 	}
 	
 }
