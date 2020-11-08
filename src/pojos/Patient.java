19ba41b7dae06447dd7818dc6dc25c015bc7c152
Jaime <jimmyviniegra@gmail.com>
2020-11-08 16:42:38 +0100

    Added dni and password to Patient

--- src/pojos/Patient.java
+++ src/pojos/Patient.java
@@ -6,107 +6,137 @@ import java.util.List;
 
 public class Patient implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1130150939909222240L;
-	
-	
-	private Integer id;
-	private String name;
-	private Date birthDate;
-	private Sex sex;
-	private List<RiskFactor> riskFactor;
-	
-	public enum Sex {MALE, FEMALE}
-	public enum RiskFactor {ASTHMA, EPOC}//to expand
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1130150939909222240L;
 
-	
-	public Patient() {
-		super();
-	}
 
-	public Patient(Integer id, String name, Date birthDate, Sex sex, List<RiskFactor> riskFactor) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.birthDate = birthDate;
-		this.sex = sex;
-		this.riskFactor = riskFactor;
-		
-	}
+    private Integer id;
+    private String name;
+    private Date birthDate;
+    private Sex sex;
+    private List<RiskFactor> riskFactor;
+    private String dni;
+    private String password;
 
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public enum Sex {MALE, FEMALE}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Patient other = (Patient) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
+    public enum RiskFactor {ASTHMA, EPOC}//to expand
 
-	@Override
-	public String toString() {
-		return "Patient [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", sex=" + sex + ", riskFactor="
-				+ riskFactor + "]";
-	}
 
-	
-	public Integer getId() {
-		return id;
-	}
+    public Patient() {
+        super();
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Patient(Integer id, String name, Date birthDate, Sex sex, List<RiskFactor> riskFactor) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.sex = sex;
+        this.riskFactor = riskFactor;
 
-	public String getName() {
-		return name;
-	}
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Patient(Integer id, String name, Date birthDate, Sex sex, List<RiskFactor> riskFactor, String dni, String password) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.sex = sex;
+        this.riskFactor = riskFactor;
+        this.dni = dni;
+        this.password = password;
+    }
 
-	public Date getBirthDate() {
-		return birthDate;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Patient other = (Patient) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
 
-	public Sex getSex() {
-		return sex;
-	}
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", sex=" + sex +
+                ", riskFactor=" + riskFactor +
+                ", dni='" + dni + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 
-	public void setSex(Sex sex) {
-		this.sex = sex;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public List<RiskFactor> getRiskFactor() {
-		return riskFactor;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setRiskFactor(List<RiskFactor> riskFactor) {
-		this.riskFactor = riskFactor;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public List<RiskFactor> getRiskFactor() {
+        return riskFactor;
+    }
+
+    public void setRiskFactor(List<RiskFactor> riskFactor) {
+        this.riskFactor = riskFactor;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
