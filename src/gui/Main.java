39a70f90fec5802547288ea933e0ca067e7bc36f
lauu2398 <l.cobena@usp.ceu.es>
2020-11-14 19:06:07 +0100

    format

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -12,68 +12,63 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
-	
+
 	private Stage window;
 	private BorderPane rootLayout;
-	
 
 	@Override
 	public void start(Stage stage) {
-		this.window=stage;
+		this.window = stage;
 		try {
 
-			this.window=stage;
-			
-			Parent root=FXMLLoader.load(getClass().getResource("/gui/MainWindow.fxml"));
+			this.window = stage;
+
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/MainWindow.fxml"));
 			this.window.setScene(new Scene(root));
 			this.window.setResizable(true);
 			this.window.show();
-			
-			this.window.setOnCloseRequest(e->closeConnection());
+
+			this.window.setOnCloseRequest(e -> closeConnection());
 			SQLManager.connect("jdbc:sqlite:././Database/covid watchlist.db");
-		
+
 			loadLogin();
-		}catch(SQLException | ClassNotFoundException | IOException e) {
+		} catch (SQLException | ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public void loadLogin() {
-		
-		
+
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainWindow.fxml"));	
-			rootLayout =  loader.load();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainWindow.fxml"));
+			rootLayout = loader.load();
 			MainWindow controller = loader.getController();
-			//controller.setMain(this);
+			// controller.setMain(this);
 			this.window.setScene(new Scene(rootLayout));
 			this.window.setResizable(true);
 			this.window.show();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("There was some kind of error and we haven't even started!");
 		}
-		
-		
+
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public void updateScene(Scene scene) {
-		
+
 		window.setScene(scene);
-		
+
 	}
-	
-	private void closeConnection(){
+
+	private void closeConnection() {
 		try {
-			
+
 			SQLManager.disconnect();
 		} catch (SQLException e) {
 			e.printStackTrace();
--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -26,49 +26,50 @@ public class MainWindow {
 	private String dni;
 	private Patient extractedPatient;
 	private String password;
-    @FXML
-    private TextField userField;
+	@FXML
+	private TextField userField;
 
-    @FXML
-    private PasswordField passField;
+	@FXML
+	private PasswordField passField;
 
-    @FXML
-    private Button signInButton;
+	@FXML
+	private Button signInButton;
 
-    @FXML
-    void signInClicked(ActionEvent event) {
-    	dni = userField.getText();
-    	password = passField.getText();
-    	this.stage=new Stage();
-    	
+	@FXML
+	void signInClicked(ActionEvent event) {
+		dni = userField.getText();
+		password = passField.getText();
+		this.stage = new Stage();
+
 		try {
-			if(checkExistance()){
-				if(checkPassword()) {
-	    	FXMLLoader loaderPatient=new FXMLLoader(getClass().getResource("/gui/PatientPanel.fxml"));
-	    	try {
-	    		BorderPane panel= loaderPatient.load();
-				PatientWindow controller=loaderPatient.<PatientWindow>getController();
-				main.updateScene(new Scene(panel));
-				//controller.setMainWindow(this);
-			
-		} catch (IOException e) {
-			Alert alert=new Alert(AlertType.ERROR, "Error loading the patient view");
+			if (checkExistance()) {
+				if (checkPassword()) {
+					FXMLLoader loaderPatient = new FXMLLoader(getClass().getResource("/gui/PatientPanel.fxml"));
+					try {
+						BorderPane panel = loaderPatient.load();
+						PatientWindow controller = loaderPatient.<PatientWindow>getController();
+						main.updateScene(new Scene(panel));
+						// controller.setMainWindow(this);
+
+					} catch (IOException e) {
+						Alert alert = new Alert(AlertType.ERROR, "Error loading the patient view");
+						alert.showAndWait();
+					}
+				} else {
+					Alert alert = new Alert(AlertType.ERROR, "Wrong username or password");
+					alert.showAndWait();
+				}
+			} else {
+				Alert alert = new Alert(AlertType.ERROR, "Wrong username or password");
+				alert.showAndWait();
+			}
+		} catch (SQLException e) {
+			Alert alert = new Alert(AlertType.ERROR, "Error in the database");
 			alert.showAndWait();
 		}
-			}else {
-				Alert alert=new Alert(AlertType.ERROR, "Wrong username or password");
-				alert.showAndWait();
-				
-			}else {
-				Alert alert=new Alert(AlertType.ERROR, "Wrong username or password");
-				alert.showAndWait();
-			}
-			}catch (SQLException e) {
-				Alert alert=new Alert(AlertType.ERROR, "Error in the database");
-				alert.showAndWait();
-			}
 
-    }
+	}
+
 	public Stage getStage() {
 		return stage;
 	}
@@ -76,44 +77,40 @@ public class MainWindow {
 	public void setStage(Stage stage) {
 		this.stage = stage;
 	}
-	
+
 	public void setMain(Main main) {
-		
+
 		this.main = main;
-		
+
 	}
-	
+
 	public void logout() {
-		
+
 		main.loadLogin();
-		
+
 	}
-	private boolean checkPassword () {
-		if(extractedPatient != null) {
-			if(extractedPatient.getPassword().equals(password)) {
+
+	private boolean checkPassword() {
+		if (extractedPatient != null) {
+			if (extractedPatient.getPassword().equals(password)) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
 		}
-		return false; 
+		return false;
 	}
-	
-	private boolean checkExistance() throws SQLException{
-	
-			extractedPatient = SQLManager.searchPatientByDni(dni);
-			
-			if(extractedPatient == null) {
-				return false;
-			}else {
-				return true;
-			}
 
-		
+	private boolean checkExistance() throws SQLException {
+
+		extractedPatient = SQLManager.searchPatientByDni(dni);
+
+		if (extractedPatient == null) {
+			return false;
+		} else {
+			return true;
+		}
+
 	}
-	
-    
-	
 
 }
-
