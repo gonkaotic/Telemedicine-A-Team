465332547b5f9cc851838bcd43de1d01bd7e1565
Lola Ardura <lolaardura99@gmail.com>
2020-11-18 09:41:45 +0100

    measurementsPanel added symptoms and ecg graph (no functionality yet)

--- src/gui/NewMeasurementPanelController.java
+++ src/gui/NewMeasurementPanelController.java
@@ -3,15 +3,20 @@ package gui;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import network.client.BitalinoHandler;
 
 public class NewMeasurementPanelController {
 
     private BitalinoHandler bitalino;
+
+    @FXML
+    private GridPane measurementsPanel;
 
     @FXML
     private Label TemperatureLabel;
@@ -26,9 +31,6 @@ public class NewMeasurementPanelController {
     private Label oxygenLabel;
 
     @FXML
-    private TextField temperatureTextField;
-
-    @FXML
     private JFXTextField heartRateTextField;
 
     @FXML
@@ -41,6 +43,9 @@ public class NewMeasurementPanelController {
     private CheckBox coughCheckBox;
 
     @FXML
+    private TextField temperatureTextField;
+
+    @FXML
     private Label breathLabel;
 
     @FXML
@@ -49,13 +54,53 @@ public class NewMeasurementPanelController {
     @FXML
     private Button recordOxygenButton;
 
+    @FXML
+    private Label tirednessLabel;
+
+    @FXML
+    private CheckBox tirednessCheckBox;
+
+    @FXML
+    private Label smellLabel;
+
+    @FXML
+    private CheckBox smellCheckBox;
+
+    @FXML
+    private Label tasteLabel;
+
+    @FXML
+    private CheckBox tasteCheckBox;
+
+    @FXML
+    private Label ecgLabel;
+
+    @FXML
+    private Button recordEcgButton;
+
+    @FXML
+    private LineChart<?, ?> ecgGraph;
+
+    @FXML
+    private Label chestPainLabel;
+
+    @FXML
+    private CheckBox chestPainCheckBox;
+
+
     public void initComponents (){
         bitalino = new BitalinoHandler("20:17:09:18:49:21");
     }
 
+    /**
+     * Records the heart rate and oxygen saturation and displays them in their corresponding textfields
+     * To block the rest of components we add a progress bar.
+     * @param event
+     *          button clicked
+     */
     @FXML
     void recordOxygenClicked(ActionEvent event) {
-        //Maybe we should block the rest of the fields
+        //Maybe we should block the rest of the fields: ¿progress bar?
         try {
             float[] pulseoximeter = bitalino.recordPulseOximeter();
             this.heartRateTextField.setText(""+pulseoximeter[0]);
@@ -64,6 +109,11 @@ public class NewMeasurementPanelController {
             //This should be a pop up or somthing similar
             System.out.println("Problems when connecting with BITalino");
         }
+    }
+
+    @FXML
+    void recordEcgClicked(ActionEvent event) {
+
     }
 
 
