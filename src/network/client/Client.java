679d7967296cf5876c946132124953662066418c
gonkaotic <30638707+gonkaotic@users.noreply.github.com>
2020-12-01 18:07:44 +0100

    Rework of client + addition of Network tester + debugged the ConnectionServer

--- src/network/client/Client.java
+++ src/network/client/Client.java
@@ -19,12 +19,9 @@ import pojos.Patient;
 public class Client implements Network {
 
 	private String serverIP;
-	private Socket socket;
-	OutputStream outputStream = null;
-	InputStream inputStream = null;
-	ObjectOutputStream objectOutputStream = null;
-	ObjectInputStream objectInputStream = null;
-	NetworkMessage msg;
+	private Socket socket = null;
+	private ObjectOutputStream objectOutputStream = null;
+	private ObjectInputStream objectInputStream = null;
 
 	/**
 	 * Client constructor
@@ -44,26 +41,77 @@ public class Client implements Network {
 		try {
 			this.socket = new Socket(serverIP, SERVERPORT);
 
-			outputStream = socket.getOutputStream();
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectInputStream = new ObjectInputStream(socket.getInputStream());
+
 			return true;
 		} catch (UnknownHostException e) {
-			System.out.println("It was not possible to connect to the server.");
+			System.out.println("Unknown server");
 			e.printStackTrace();
+			releaseResources(objectInputStream, objectOutputStream, socket );
 			return false;
 		} catch (IOException e) {
 			System.out.println("It was not possible to connect to the server.");
 			e.printStackTrace();
+			releaseResources(objectInputStream, objectOutputStream, socket );
 			return false;
 		}
 	}
+
+	/**
+	 *  This would be the first message a client should sent to the server to "log in"
+	 * @returns the patient with the measurements if the id and password are correct, null otherwise.
+	 * @param patient A patient that should have DNI and password in it
+	 */
+	public Patient sendToServer ( Patient patient ) throws IOException, ClassNotFoundException {
+
+		NetworkMessage msg = new NetworkMessage( NetworkMessage.Protocol.GET_PATIENT, patient);
+
+		System.out.println("socket: "+ socket.getInetAddress());
+
+		//objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		//objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+		if ( objectOutputStream != null ) {
+			objectOutputStream.writeObject( msg );
+			objectOutputStream.flush();
+			NetworkMessage answer = (NetworkMessage) objectInputStream.readObject();
+
+			if ( answer.getProtocol() == NetworkMessage.Protocol.PUSH_PATIENT ){
+				return answer.getPatient();
+			}
+			/*
+			else if ( answer.getProtocol() == NetworkMessage.Protocol.DENY_PATIENT ){
+				return null;
+			}
+			*/
+		}
+		return null;
+	}
+	/**
+	 *  Sends the measurements to save in the database.
+	 *  If the client hasn't logged in with the previous message there will be an error
+	 * @param  measurements list of measurements to insert in the database
+	 */
+	public void sendToServer ( ArrayList<Measurement> measurements ) throws IOException {
+		NetworkMessage msg = new NetworkMessage( NetworkMessage.Protocol.PUSH_MEASUREMENT, measurements );
+		objectOutputStream.writeObject( msg );
+	}
+
 
 	/**
 	 * Disconnect from server and release resources
 	 */
 	public void disconnect() {
 		// Release input and output stream
-		releaseResources(objectInputStream, objectOutputStream, socket);
-		// I would define a command to be sent to the server
+		try {
+			NetworkMessage msg = new NetworkMessage(NetworkMessage.Protocol.DISCONNECT);
+			objectOutputStream.writeObject( msg );
+		} catch ( IOException e){
+			e.printStackTrace();
+		} finally {
+			releaseResources(objectInputStream, objectOutputStream, socket);
+		}
 
 	}
 
@@ -77,7 +125,7 @@ public class Client implements Network {
 				objectInputStream = new ObjectInputStream(inputStream);
 
 				// communication with server
-				objectOutputStream.writeObject(msg);
+				//objectOutputStream.writeObject(msg);
 				objectOutputStream.flush();// just in case
 
 				NetworkMessage msg_read = (NetworkMessage) objectInputStream.readObject();
@@ -90,72 +138,51 @@ public class Client implements Network {
 					ArrayList<Measurement> measures = msg_read.getMeasurements();
 					System.out.println("Measurement received"+ measures);
 					
-				}else if (msg.getProtocol() == NetworkMessage.Protocol.DISCONNECT) {
+				}else if (msg_read.getProtocol() == NetworkMessage.Protocol.DISCONNECT) {
 					System.out.println("Finish");
-					releaseResources3(outputStream, socket);
+					releaseResources(inputStream, outputStream, socket);
 					System.exit(0);
 				}
 
-				
+
 
 			} catch (IOException ex) {
 				System.out.println("It is not possible to connect with the Server");
 				Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
 			} finally {
-				releaseResources2(objectOutputStream, socket);
-
+				releaseResources( objectInputStream, objectOutputStream, socket );
 			}
 
 			return true;
 		}
 	}
 
-	private static void releaseResources(ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream,
+	//passing the current methods is kind of an overkill, as they are global variables, but lets work like that
+	private static void releaseResources(InputStream input, OutputStream output,
 			Socket socket) {
-		try {
-			objectInputStream.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		try {
-			objectOutputStream.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		try {
-			socket.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+
+		if( input != null) {
+			try {
+				input.close();
+			} catch (IOException ex) {
+				Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+			}
 		}
 
-	}
-
-	private static void releaseResources2(ObjectOutputStream objectOutputStream, Socket socket) {
-
-		try {
-			objectOutputStream.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		try {
-			socket.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+		if ( output != null ) {
+			try {
+				output.close();
+			} catch (IOException ex) {
+				Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+			}
 		}
 
-	}
-
-	private static void releaseResources3(OutputStream outputStream, Socket socket) {
-
-		try {
-			outputStream.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		try {
-			socket.close();
-		} catch (IOException ex) {
-			Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+		if ( socket != null ) {
+			try {
+				socket.close();
+			} catch (IOException ex) {
+				Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+			}
 		}
 
 	}
