a0808ea70921dc582c7469537f246d9548efc1cf
lauu2398 <l.cobena@usp.ceu.es>
2020-12-23 20:07:41 +0100

    3 intento

--- src/gui/IpLoginTest.java
+++ src/gui/IpLoginTest.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class IPLoginTest extends Application {
+public class IpLoginTest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
