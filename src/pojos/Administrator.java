9feec2c648277702ec32a7c0dd0365a5b7a5b552
gonkaotic <30638707+gonkaotic@users.noreply.github.com>
2020-12-23 20:31:33 +0100

    Created new pojos so I can start working, Jaime you have to use the magic fingers that I know you have to complete those.

---
+++ src/pojos/Administrator.java
@@ -0,0 +1,28 @@
+package pojos;
+
+public class Administrator {
+
+
+    private String dni;
+    private String password;
+
+    public Administrator ( ) {
+
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/pojos/Doctor.java
@@ -0,0 +1,29 @@
+package pojos;
+
+public class Doctor {
+
+    private String dni;
+    private String password;
+
+
+    public Doctor (){
+
+    }
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
