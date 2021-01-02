2db5efa0e1704454905e3e3fdaebdbfe110679a7
Lola Ardura <lolaardura99@gmail.com>
2021-01-02 17:50:59 +0100

    adminMain: display menu, connect to server and almost login

---
+++ src/network/AdminClient/AdminMain.java
@@ -0,0 +1,103 @@
+package network.AdminClient;
+
+import network.Network;
+import pojos.Administrator;
+import security.PasswordAuthentication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * The admin app is a console-based application
+ */
+public class AdminMain {
+    private static BufferedReader console = null;
+    private static AdminClient adminClient;
+
+    public static void main(String[] args) {
+        console = new BufferedReader(new InputStreamReader(System.in));
+        while (!connectToServer()) ;
+
+
+        /*
+        while(true){
+            int option = displayMenu();
+        }
+
+         */
+
+    }
+
+    /**
+     * displays the admin menu is console
+     *
+     * @return the option's number or -1 if an invalid entry was specified
+     */
+    private static int displayMenu() {
+        System.out.println("---------------- MENU ----------------\n" +
+                "1. Register patient\n" +
+                "2. Register doctor\n" +
+                "3. Register admin\n" +
+                "4. Shutdown server\n" +
+                "5. Exit");
+
+        System.out.print("Enter a number indicating the task to perform: ");
+        try {
+            String reading = console.readLine();
+            int option = Integer.parseInt(reading);
+            return option;
+        } catch (IOException e) {
+            System.out.println("Error during reading");
+            return -1;
+        } catch (NumberFormatException ex) {
+            System.out.println("Invalid option");
+            return -1;
+        }
+    }
+
+    /**
+     * Starts connection with the server by opening a socket against the server
+     *
+     * @return true if everything went ok, false if an error occurred throughout the process
+     */
+    private static boolean connectToServer() {
+        try {
+            System.out.print("Introduce the server's IP: ");
+            String serverIp = console.readLine();
+            boolean validIP = serverIp.matches("((([01]?([0-9]){1,2})|(2[0-4][0-9])|(25[0-5]))\\.){3}(([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))") ||
+                    serverIp.matches("localhost");
+            while (!validIP) {
+                System.out.print("\nIncorrect IP address. Please enter a valid address: ");
+                serverIp = console.readLine().trim();
+                validIP = serverIp.matches("((([01]?([0-9]){1,2})|(2[0-4][0-9])|(25[0-5]))\\.){3}(([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))") ||
+                        serverIp.matches("localhost");
+            }
+
+            adminClient = new AdminClient(serverIp);
+            boolean connected = adminClient.connect();
+            return connected;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Problems with the console");
+            return false;
+        }
+    }
+
+    private static void login() {
+        try {
+            System.out.print("DNI: ");
+            String dni = console.readLine();
+            System.out.println("Password: ");
+            String password = console.readLine();
+
+            Administrator admin = new Administrator(dni,password);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
