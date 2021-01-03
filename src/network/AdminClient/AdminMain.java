f832b4705b1c44f21d476e1a9fcc865295db35ac
Lola Ardura <lolaardura99@gmail.com>
2021-01-03 12:10:05 +0100

    register patient: collected dni, password and sex

--- src/network/AdminClient/AdminMain.java
+++ src/network/AdminClient/AdminMain.java
@@ -6,6 +6,7 @@ import pojos.Administrator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Date;
 
 
 /**
@@ -19,15 +20,17 @@ public class AdminMain {
     public static void main(String[] args) {
         console = new BufferedReader(new InputStreamReader(System.in));
         boolean login=false;
+        while (!connectToServer()) ;
         do {
-            while (!connectToServer()) ;
             login = login();
         }while (!login);
 
         while(true){
             int option = displayMenu();
             switch (option){
-                case 1:break;
+                case 1:
+                    registerPatient();
+                    break;
                 case 2: break;
                 case 3: break;
                 case 4: break;
@@ -46,7 +49,7 @@ public class AdminMain {
     }
 
     /**
-     * displays the admin menu is console
+     * displays the admin menu in console
      *
      * @return the option's number or -1 if an invalid entry was specified
      */
@@ -125,6 +128,90 @@ public class AdminMain {
             return false;
         } catch (ProtocolException e) {
             e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void registerPatient() {
+        try {
+            System.out.print("\n ------ PATIENT INFO ------- " +
+                    "\nYou can type x at any time to cancel the resgistry\n");
+
+            System.out.print("\nDNI: ");
+            String dni = console.readLine();
+            if(exitOption(dni)) return;
+            while (!verifyDNI(dni)) {
+                System.out.print("\nInvalid DNI (should be 8 digits and a letter)");
+                System.out.print("\nDNI: ");
+                dni = console.readLine();
+                if(exitOption(dni)) return;
+            }
+
+            System.out.print("\nPassword: ");
+            String password = console.readLine();
+            if(exitOption(password)) return;
+
+
+            System.out.print("\nSex (m/f): ");
+            String sex = console.readLine();
+            while(!verifySex(sex)){
+                System.out.println("Invalid sex. Only m/f options are allowed");
+                System.out.print("Sex (m/f): ");
+                sex = console.readLine();
+                if(exitOption(sex)) return;
+            }
+
+            System.out.print("\nDate of birth (YYYY-MM-DD):");
+            String date = console.readLine();
+            while(!verifyDate(date)){
+
+            }
+
+            System.out.println("Select one of the following doctors (enter his DNI):");
+            //TODO: Print all doctors and get one
+
+            System.out.println("From the risk factors below select y/n");
+            System.out.print("Cancer (y/n): ");
+            System.out.print("\nChronic Kidney Disease (y/n): ");
+            System.out.print("\nChronic Obstructive Pulmonary Disease (y/n): ");
+            System.out.print("\nHeart Conditions (y/n): ");
+            System.out.print("\nInmunocompromised (y/n): ");
+            System.out.print("\nObesity (y/n): ");
+            System.out.print("\nSmoker (y/n): ");
+            System.out.print("\nPregnancy (y/n): ");
+            System.out.print("\nDiabetes Type 2 (y/n): ");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Verifies that DNI is a combination of 8 numbers and a letter: does not evaluate that the letter is correct
+     * @param dni
+     * @return
+     *      true if verification succeeded
+     *      false otherwise
+     */
+    private static boolean verifyDNI(String dni){
+        return dni.matches("[0-9]{8}[a-zA-Z]");
+    }
+
+    private static boolean verifySex(String sex){
+        return sex.equalsIgnoreCase("f")||sex.equalsIgnoreCase("m");
+    }
+
+
+    private static boolean exitOption(String reading){
+        return (reading.equalsIgnoreCase("x"));
+    }
+
+    private static boolean verifyDate(String date){
+        try{
+            Date.valueOf(date);
+            return true;
+        }catch(IllegalArgumentException ex){
             return false;
         }
     }
